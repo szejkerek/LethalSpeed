0a965e3dbdc73d50bceb7a023979b780bc8c2c33
Szejkerek <bartekk.gordon@gmail.com>
2023-03-29 16:58:57 +0200

    Add bullet script

--- Assets/_Scripts/Enemy.cs
+++ Assets/_Scripts/Enemy.cs
@@ -31,19 +31,9 @@ public class Enemy : MonoBehaviour
     {
         if(_playerInRange)
         {
-            // Determine which direction to rotate towards
             Vector3 targetDirection = _player.transform.position - transform.position;
-
-            // The step size is equal to speed times frame time.
             float singleStep = _rotationSpeed * Time.deltaTime;
-
-            // Rotate the forward vector towards the target direction by one step
             Vector3 newDirection = Vector3.RotateTowards(transform.forward, targetDirection, singleStep, 0.0f);
-
-            // Draw a ray pointing at our target in
-            Debug.DrawRay(transform.position, newDirection, Color.red);
-
-            // Calculate a rotation a step closer to the target and applies rotation to this object
             transform.rotation = Quaternion.LookRotation(newDirection);
         }    
     }
