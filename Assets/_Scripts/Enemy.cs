8125810f7c2147b673f0ddc2218c09fe3219669d
Szejkerek <bartekk.gordon@gmail.com>
2023-03-29 16:03:56 +0200

    Enemy rotate to player

--- Assets/_Scripts/Enemy.cs
+++ Assets/_Scripts/Enemy.cs
@@ -1,18 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 public class Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Shooting")]
+    [SerializeField] private Transform _shootingPoint;
+    [SerializeField] private float _rotationSpeed = 10f;
+    [SerializeField] private float _triggerRange = 15f;
+
+    private Player _player;
+    private bool _playerInRange = false;
+    void TryToFindPlayer()
     {
-        
+        _player = FindObjectOfType<Player>();    
+
+        if(_player is null)
+        {
+            Debug.LogWarning("Player not found");
+        }
     }
 
-    // Update is called once per frame
+    void CheckIfPlayerInRange()
+    {
+        _playerInRange = Vector3.Distance(transform.position, _player.transform.position) <= _triggerRange;
+    }
+
+    void RotateTowardsPlayer()
+    {
+        if(_playerInRange)
+        {
+            // Determine which direction to rotate towards
+            Vector3 targetDirection = _player.transform.position - transform.position;
+
+            // The step size is equal to speed times frame time.
+            float singleStep = _rotationSpeed * Time.deltaTime;
+
+            // Rotate the forward vector towards the target direction by one step
+            Vector3 newDirection = Vector3.RotateTowards(transform.forward, targetDirection, singleStep, 0.0f);
+
+            // Draw a ray pointing at our target in
+            Debug.DrawRay(transform.position, newDirection, Color.red);
+
+            // Calculate a rotation a step closer to the target and applies rotation to this object
+            transform.rotation = Quaternion.LookRotation(newDirection);
+        }    
+    }
+
+    void Start()
+    {
+        TryToFindPlayer();
+    }
+
     void Update()
     {
-        
+        CheckIfPlayerInRange();
+        RotateTowardsPlayer();
     }
 }
