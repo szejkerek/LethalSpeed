ed6ff3e9cf8c0e20dd7f6d007faddeffce5cc5a7
sztynksior <szszedel@gmail.com>
2023-03-29 15:18:42 +0200

    added temporary scenes and scene enum

--- Assets/_Scripts/SceneLoader.cs
+++ Assets/_Scripts/SceneLoader.cs
@@ -1,18 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
 
 public class SceneLoader : Singleton<SceneLoader>
 {
+    public Slider ProgressBar;
+
     // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
         
     }
 
     // Update is called once per frame
-    void Update()
+    private void LoadGame()
     {
         
+        //SceneManager.LoadSceneAsync(PersistentSingleton)
     }
 }
