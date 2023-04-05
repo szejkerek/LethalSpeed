2b5b8a1ad2535cc6babf1738493d3b2c6ff8381a
Paweł Kupczak <pawel.kupczak.sc@gmail.com>
2023-04-05 14:03:14 +0200

    Allow to enter a slide with an overspeed, and decrease it slowly (helps to preserve momentum)

--- Assets/_Scripts/Movement/SlidingState.cs
+++ Assets/_Scripts/Movement/SlidingState.cs
@@ -4,7 +4,14 @@ public class SlidingState : MovementState
 {
     private PlayerMovement _pm;
     private RaycastHit _slopeRayHit;
+    private bool _wasOnSlope;
     private float _slidingTime;
+    private float _initialSpeed;
+
+    public SlidingState(float initialSpeed)
+    {
+        _initialSpeed = initialSpeed;
+    }
 
     public void Begin(PlayerMovement pm)
     {
@@ -18,22 +25,44 @@ public class SlidingState : MovementState
     public void Update()
     {
         _slidingTime -= Time.deltaTime;
-
         ClipSlidingSpeed();
+
+        if (_initialSpeed > _pm.MaxSpeed)
+        {
+            float currectVelocity = _pm.FlatVelocity.magnitude;
+
+            _initialSpeed = currectVelocity < _pm.MaxSpeed ? _pm.MaxSpeed : currectVelocity;
+            _pm.Rigidbody.drag = 0.0f;
+        }
+        else if(!_pm.IsOnSlope(out _slopeRayHit) && _wasOnSlope && _pm.FlatVelocity.magnitude > _pm.MaxSpeed)
+        {
+            _initialSpeed = _pm.FlatVelocity.magnitude;
+            _pm.Rigidbody.drag = 0.0f;
+        }
+        else
+        {
+            _pm.Rigidbody.drag = _pm.GroundFriction;
+        }
+
+        _wasOnSlope = _pm.IsOnSlope(out _slopeRayHit);
         CheckForModeChange();
     }
 
     public void Move(Vector3 normalizedWishDir)
     {
-        if (_pm.IsOnSlope(out _slopeRayHit) && _pm.Rigidbody.velocity.y < -0.1f)
+        if (_pm.IsOnSlope(out _slopeRayHit) && _pm.Velocity.y < -0.1f)
         {
             float slopeUpAngle = Vector3.Angle(Vector3.up, _slopeRayHit.normal);
 
-            _pm.Rigidbody.AddForce(Vector3.ProjectOnPlane(normalizedWishDir, _slopeRayHit.normal).normalized * _pm.MaxSpeed * _pm.SlidingAcceleration * (1.0f + slopeUpAngle / _pm.MaxSlopeAngle), ForceMode.Force);
+            _pm.Rigidbody.AddForce(
+                Vector3.ProjectOnPlane(normalizedWishDir, _slopeRayHit.normal).normalized * _pm.MaxSpeed * _pm.SlidingAcceleration 
+                * (1.0f + slopeUpAngle / _pm.MaxSlopeAngle),
+                ForceMode.Force
+            );
             _pm.Rigidbody.useGravity = false;
 
             _slidingTime = _pm.MaxSlidingTimeInSeconds;
-
+            
             return;
         }
 
@@ -53,21 +82,21 @@ public class SlidingState : MovementState
     {
         if (Input.GetKeyDown(_pm.JumpKey))
         {
-            _pm.Rigidbody.velocity = new Vector3(_pm.Rigidbody.velocity.x, 0.0f, _pm.Rigidbody.velocity.z);
+            _pm.Velocity = _pm.FlatVelocity;
             _pm.Rigidbody.AddForce(Vector3.up * _pm.JumpForce * _pm.SlideJumpForce, ForceMode.Impulse);
-            _pm.ChangeMovementState(new AirState(new Vector3(_pm.Rigidbody.velocity.x, 0.0f, _pm.Rigidbody.velocity.z).magnitude));
+            _pm.ChangeMovementState(new AirState(_pm.FlatVelocity.magnitude));
 
             return;
         }
 
         if (!_pm.IsGrounded && _pm.WasGrounded)
         {
-            _pm.ChangeMovementState(new AirState(new Vector3(_pm.Rigidbody.velocity.x, 0.0f, _pm.Rigidbody.velocity.z).magnitude));
+            _pm.ChangeMovementState(new AirState(_pm.FlatVelocity.magnitude));
 
             return;
         }
         
-        if(_pm.Rigidbody.velocity.magnitude < 1.0f || _slidingTime <= 0.0f)
+        if(_pm.Velocity.magnitude < 1.0f || _slidingTime <= 0.0f)
         {
             _pm.ChangeMovementState(new CrouchingState());
 
@@ -84,22 +113,32 @@ public class SlidingState : MovementState
 
     private void ClipSlidingSpeed()
     {
-        if(_pm.IsOnSlope(out _slopeRayHit) && _pm.Rigidbody.velocity.y < -0.1f)
+        if(_pm.IsOnSlope(out _slopeRayHit) && _pm.Velocity.y < -0.1f)
         {
-            _pm.velocityText.text = $"Down sliding velocity: {_pm.Rigidbody.velocity.magnitude:0.##}ups - Y vel: {_pm.Rigidbody.velocity.y:0.##}";
+            _pm.velocityText.text = $"Down sliding velocity: {_pm.Velocity.magnitude:0.##}ups - Y vel: {_pm.Velocity.y:0.##}";
 
             return;
         }
 
-        Vector3 flatVel = new Vector3(_pm.Rigidbody.velocity.x, 0.0f, _pm.Rigidbody.velocity.z);
-
-        if (flatVel.magnitude > _pm.MaxSpeed)
+        if(_initialSpeed > _pm.MaxSpeed)
         {
-            Vector3 newSpeed = flatVel.normalized * _pm.MaxSpeed;
+            float drop = _pm.FlatVelocity.magnitude - _pm.MaxSpeed > 1.0f ? 
+                _pm.GroundDeacceleration * Time.deltaTime / 5.0f : _pm.FlatVelocity.magnitude - _pm.MaxSpeed;
 
-            _pm.Rigidbody.velocity = new Vector3(newSpeed.x, _pm.Rigidbody.velocity.y, newSpeed.z);
+            Vector3 newSpeed = _pm.FlatVelocity.normalized * Mathf.Min(_initialSpeed, _pm.FlatVelocity.magnitude - drop);
+
+            _pm.Velocity = new Vector3(newSpeed.x, _pm.Velocity.y, newSpeed.z);
+        }
+        else if (_pm.FlatVelocity.magnitude > _pm.MaxSpeed)
+        {
+            float drop = _pm.FlatVelocity.magnitude - _pm.MaxSpeed > 1.0f ?
+                _pm.GroundDeacceleration * Time.deltaTime : _pm.FlatVelocity.magnitude - _pm.MaxSpeed;
+
+            Vector3 newSpeed = _pm.FlatVelocity.normalized * (_pm.FlatVelocity.magnitude - drop);
+
+            _pm.Velocity = new Vector3(newSpeed.x, _pm.Velocity.y, newSpeed.z);
         }
 
-        _pm.velocityText.text = $"Sliding velocity: {flatVel.magnitude:0.##}ups - Y vel: {_pm.Rigidbody.velocity.y:0.##}";
+        _pm.velocityText.text = $"Sliding velocity: {_pm.FlatVelocity.magnitude:0.##}ups - Y vel: {_pm.Velocity.y:0.##}";
     }
 }
