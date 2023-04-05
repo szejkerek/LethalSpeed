a13d83f16486f5211b40f64988fecfc521526028
Paweł Kupczak <pawel.kupczak.sc@gmail.com>
2023-04-05 13:05:51 +0200

    Add slope movement

--- Assets/_Scripts/Movement/RunningState.cs
+++ Assets/_Scripts/Movement/RunningState.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class RunningState : MovementState
 {
     private PlayerMovement _pm;
+    private RaycastHit _slopeRayHit;
 
     public void Begin(PlayerMovement pm)
     {
@@ -19,7 +20,16 @@ public class RunningState : MovementState
 
     public void Move(Vector3 normalizedWishDir)
     {
+        if (_pm.IsOnSlope(out _slopeRayHit))
+        {
+            _pm.Rigidbody.AddForce(Vector3.ProjectOnPlane(normalizedWishDir, _slopeRayHit.normal).normalized * _pm.MaxSpeed * _pm.GroundAcceleration, ForceMode.Force);
+            _pm.Rigidbody.useGravity = false;
+
+            return;
+        }
+
         _pm.Rigidbody.AddForce(normalizedWishDir * _pm.MaxSpeed * _pm.GroundAcceleration, ForceMode.Force);
+        _pm.Rigidbody.useGravity = true;
     }
 
     public void End()
@@ -33,14 +43,14 @@ public class RunningState : MovementState
         {
             _pm.Rigidbody.velocity = new Vector3(_pm.Rigidbody.velocity.x, 0.0f, _pm.Rigidbody.velocity.z);
             _pm.Rigidbody.AddForce(Vector3.up * _pm.JumpForce, ForceMode.Impulse);
-            _pm.ChangeMovementState(new AirState());
+            _pm.ChangeMovementState(new AirState(new Vector3(_pm.Rigidbody.velocity.x, 0.0f, _pm.Rigidbody.velocity.z).magnitude));
 
             return;
         }
 
         if (!_pm.IsGrounded && _pm.WasGrounded)
         {
-            _pm.ChangeMovementState(new AirState());
+            _pm.ChangeMovementState(new AirState(new Vector3(_pm.Rigidbody.velocity.x, 0.0f, _pm.Rigidbody.velocity.z).magnitude));
 
             return;
         }
@@ -55,6 +65,14 @@ public class RunningState : MovementState
 
     private void ClipGroundSpeed()
     {
+        if (_pm.IsOnSlope(out _slopeRayHit) && _pm.Rigidbody.velocity.magnitude > _pm.MaxSpeed)
+        {
+            _pm.Rigidbody.velocity = _pm.Rigidbody.velocity.normalized * _pm.MaxSpeed;
+            _pm.velocityText.text = $"Slope velocity: {_pm.Rigidbody.velocity.magnitude:0.##}ups - Y vel: {_pm.Rigidbody.velocity.y:0.##}";
+
+            return;
+        }
+
         Vector3 flatVel = new Vector3(_pm.Rigidbody.velocity.x, 0.0f, _pm.Rigidbody.velocity.z);
 
         if(flatVel.magnitude > _pm.MaxSpeed)
