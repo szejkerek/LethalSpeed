766860199bb054cdc341cbd32665115d336b36b9
Paweł Kupczak <pawel.kupczak.sc@gmail.com>
2023-04-05 17:20:53 +0200

    Add methods to interpolate fov and z tilt

--- Assets/_Scripts/PlayerCam.cs
+++ Assets/_Scripts/PlayerCam.cs
@@ -1,6 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class PlayerCam : MonoBehaviour
 {
@@ -10,12 +9,17 @@ public class PlayerCam : MonoBehaviour
 
     [Space]
     public Transform orientation;
+    public Transform camHolder;
+
+    private Camera _camera;
 
     private float _xRot;
     private float _yRot;
  
     void Start()
     {
+        _camera = GetComponent<Camera>();
+
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
     }
@@ -29,7 +33,17 @@ public class PlayerCam : MonoBehaviour
         _xRot -= mouseY;
         _xRot = Mathf.Clamp(_xRot, -90.0f, 90.0f);
 
-        transform.rotation = Quaternion.Euler(_xRot, _yRot, 0.0f);
+        camHolder.rotation = Quaternion.Euler(_xRot, _yRot, 0.0f);
         orientation.rotation = Quaternion.Euler(0.0f, _yRot, 0.0f);
+    }
+
+    public void SetFOV(float targetFOV)
+    {
+        _camera.DOFieldOfView(targetFOV, 0.25f);
+    }
+
+    public void SetTilt(float targetTiltZ)
+    {
+        transform.DOLocalRotate(new Vector3(0.0f, 0.0f, targetTiltZ), 0.25f);
     }
 }
