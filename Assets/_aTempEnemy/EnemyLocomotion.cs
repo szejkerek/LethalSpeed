610c2f975728a74263ee12d2fbbb646ab01ae30f
Bartłomiej Gordon <bartekk.gordon@gmail.com>
2023-04-06 16:13:57 +0200

    Update animations to look more realistic

--- Assets/_aTempEnemy/EnemyLocomotion.cs
+++ Assets/_aTempEnemy/EnemyLocomotion.cs
@@ -7,14 +7,17 @@ public class EnemyLocomotion : MonoBehaviour
 {
     public Transform _player;
     NavMeshAgent _agent;
+    Animator animator;
 
     private void Awake()
     {
         _agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
     }
 
     private void Update()
     {
         _agent.SetDestination(_player.transform.position);
+        animator.SetFloat("Speed", _agent.velocity.magnitude);
     }
 }
