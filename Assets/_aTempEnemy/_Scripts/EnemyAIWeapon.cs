3b289cf43aeb0f372dec44661c60f922ed0d6a6d
Bartłomiej Gordon <bartekk.gordon@gmail.com>
2023-04-09 15:02:48 +0200

    Fix enemy rig

---
+++ Assets/_aTempEnemy/_Scripts/EnemyAIWeapon.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations.Rigging;
+
+public class EnemyAIWeapon : MonoBehaviour
+{
+    private void Awake()
+    {
+        SetUpRig();
+    }
+
+    private void SetUpRig()
+    {
+        Transform source = FindObjectOfType<PlayerCamera>().transform;
+        if (source != null)
+        {
+            foreach (MultiAimConstraint component in GetComponentsInChildren<MultiAimConstraint>())
+            {
+                var data = component.data.sourceObjects;
+                data.SetTransform(0, source);
+                component.data.sourceObjects = data;
+            }
+            RigBuilder rigs = GetComponent<RigBuilder>();
+            rigs.Build();
+        }
+        else
+        {
+            Debug.LogError("Couldn't find player");
+        }
+    }
+}
