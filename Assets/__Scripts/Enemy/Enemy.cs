89b0f9bc006e8b48278a873c9266cbbbf5f1a51c
Bartłomiej Gordon <bartekk.gordon@gmail.com>
2023-04-12 22:07:56 +0200

    Move rig's setup functionality

--- Assets/__Scripts/Enemy/Enemy.cs
+++ Assets/__Scripts/Enemy/Enemy.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Animations.Rigging;
 
 public class Enemy : MonoBehaviour
 {
@@ -9,6 +10,7 @@ public class Enemy : MonoBehaviour
     {
         _stateMachine = GetComponent<StateMachineEnemyAI>();
         ApplyHitboxesToLimbs();
+        SetUpRig();
     }
 
     public void Die()
@@ -32,6 +34,26 @@ public class Enemy : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.K))
         {
             Die();
+        }
+    }
+
+    private void SetUpRig()
+    {
+        Transform source = FindObjectOfType<Player>().PlayerCamera.EnemyAimTarget;
+        if (source != null)
+        {
+            foreach (MultiAimConstraint component in GetComponentsInChildren<MultiAimConstraint>())
+            {
+                var data = component.data.sourceObjects;
+                data.SetTransform(0, source);
+                component.data.sourceObjects = data;
+            }
+            RigBuilder rigs = GetComponent<RigBuilder>();
+            rigs.Build();
+        }
+        else
+        {
+            Debug.LogError("Couldn't find player");
         }
     }
 }
--- Assets/__Scripts/Enemy/WeaponEnemyAI.cs
+++ Assets/__Scripts/Enemy/WeaponEnemyAI.cs
@@ -5,28 +5,7 @@ using UnityEngine.Animations.Rigging;
 
 public class WeaponEnemyAI : MonoBehaviour
 {
-    private void Awake()
-    {
-        SetUpRig();
-    }
 
-    private void SetUpRig()
-    {
-        Transform source = FindObjectOfType<Player>().PlayerCamera.EnemyAimTarget;
-        if (source != null)
-        {
-            foreach (MultiAimConstraint component in GetComponentsInChildren<MultiAimConstraint>())
-            {
-                var data = component.data.sourceObjects;
-                data.SetTransform(0, source);
-                component.data.sourceObjects = data;
-            }
-            RigBuilder rigs = GetComponent<RigBuilder>();
-            rigs.Build();
-        }
-        else
-        {
-            Debug.LogError("Couldn't find player");
-        }
-    }
+
+
 }
