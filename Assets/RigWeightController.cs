69c3788e31aec967eb1f343608d6afda6b22523d
Bartłomiej Gordon <bartekk.gordon@gmail.com>
2023-04-12 22:08:09 +0200

    Create rig weight controller

---
+++ Assets/RigWeightController.cs
@@ -0,0 +1,33 @@
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.Animations.Rigging;
+
+public class RigWeightController : MonoBehaviour
+{
+    [SerializeField] private float interpolationTime = 2f;
+
+    private Rig rig;
+
+    private void Awake()
+    {
+        rig = GetComponent<Rig>();
+        ChangeRigWeightValueInTime(0f, 0f);
+    }
+    public void TurnOnRig()
+    {
+        ChangeRigWeightValueInTime(1f, interpolationTime);
+    }
+
+    public void TurnOffRig()
+    {
+        ChangeRigWeightValueInTime(0f, interpolationTime);
+    }
+
+    private void ChangeRigWeightValueInTime(float value, float interpolationTime)
+    {
+        float currentWeight = rig.weight;
+        Tween weightTween = DOTween.To(() => currentWeight, x => currentWeight = x, value, interpolationTime)
+            .SetUpdate(true)
+            .OnUpdate(() => rig.weight = currentWeight);
+    }
+}
