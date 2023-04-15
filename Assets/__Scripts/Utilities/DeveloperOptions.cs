5069b0e08c6fee986372449c3eac5008c2eaea2c
Bartłomiej Gordon <bartekk.gordon@gmail.com>
2023-04-15 20:35:31 +0200

    Add powerful dev options for states

--- Assets/__Scripts/Utilities/DeveloperOptions.cs
+++ Assets/__Scripts/Utilities/DeveloperOptions.cs
@@ -1,3 +1,4 @@
+using System.Security.Cryptography.X509Certificates;
 using UnityEditor;
 using UnityEngine;
 
@@ -18,6 +19,68 @@ public class DeveloperOptions : MonoBehaviour
             enemy.Die();
         }
     }
+
+    [MenuItem(_developerFolder + "Enemy/State/Idle")]
+    public static void IdleEnemyAI() { ChangeAllEnemiesState("Idle"); }
+
+
+    [MenuItem(_developerFolder + "Enemy/State/Patrol")]
+    public static void PatrolEnemyAI() { ChangeAllEnemiesState("Patrol"); }
+
+
+    [MenuItem(_developerFolder + "Enemy/State/Crouch")]
+    public static void CrouchEnemyAI() { ChangeAllEnemiesState("Crouch"); }
+
+
+    [MenuItem(_developerFolder + "Enemy/State/Seek")]
+    public static void SeekEnemyAI() { ChangeAllEnemiesState("Seek"); }
+
+
+    [MenuItem(_developerFolder + "Enemy/State/Shoot")]
+    public static void ShootEnemyAI() { ChangeAllEnemiesState("Shoot"); }
+
+
+    [MenuItem(_developerFolder + "Enemy/State/WalkBackward")]
+    public static void WalkBackwardEnemyAI() { ChangeAllEnemiesState("WalkBackward"); }
+
+
+    [MenuItem(_developerFolder + "Enemy/State/Ragdoll")]
+    public static void RagdollEnemyAI() { ChangeAllEnemiesState("Ragdoll"); }
+
+
+    static void ChangeAllEnemiesState(string stateName)
+    {
+        Enemy[] enemies = FindObjectsOfType<Enemy>();
+        foreach (Enemy enemy in enemies)
+        {
+            StateMachineEnemyAI stateMachine = enemy.GetComponent<StateMachineEnemyAI>();
+            switch (stateName)
+            {
+                case "Idle":
+                    stateMachine.CurrentState.SwitchState(stateMachine.StatesFactory.Idle());
+                    break;
+                case "Patrol":
+                    stateMachine.CurrentState.SwitchState(stateMachine.StatesFactory.Patrol());
+                    break;
+                case "Crouch":
+                    stateMachine.CurrentState.SwitchState(stateMachine.StatesFactory.Crouch());
+                    break;
+                case "Seek":
+                    stateMachine.CurrentState.SwitchState(stateMachine.StatesFactory.SeekPlayer());
+                    break;
+                case "Shoot":
+                    stateMachine.CurrentState.SwitchState(stateMachine.StatesFactory.ShootPlayer());
+                    break;
+                case "WalkBackward":
+                    stateMachine.CurrentState.SwitchState(stateMachine.StatesFactory.WalkBackward());
+                    break;
+                case "Ragdoll":
+                    stateMachine.CurrentState.SwitchState(stateMachine.StatesFactory.Ragdoll());
+                    break;
+            }
+        }
+    }
+
 
 #endif
 }
