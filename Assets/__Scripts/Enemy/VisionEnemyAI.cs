19873839f234cbbf1f2b21dceffdbc8e405abd37
Bartłomiej Gordon <bartekk.gordon@gmail.com>
2023-04-18 22:00:53 +0200

    Fix vector math in Enemy Vision

--- Assets/__Scripts/Enemy/VisionEnemyAI.cs
+++ Assets/__Scripts/Enemy/VisionEnemyAI.cs
@@ -26,8 +26,6 @@ public class VisionEnemyAI : MonoBehaviour
     float lastScanTime = 0;
     Collider[] colliders = new Collider[1];
     PlayerMovement playerMovment;
-    Transform scannedPlayer;
-    float scaledPlayerHeight;
     float playerHeight;
 
     private void Awake()
@@ -40,10 +38,35 @@ public class VisionEnemyAI : MonoBehaviour
     private void Update()
     {
         _lastSeenTimer += Time.deltaTime;
+        
         Scan();
     }
 
-    public void Scan()
+    Vector3 topPosition;
+    Vector3 middlePosition;
+    Vector3 bottomPosition;
+    Vector3 rightPosition;
+    Vector3 leftPosition;
+    Vector3 forwardPosition;
+    Vector3 backPosition;
+    private void UpdatePlayerBodyPartsPositions(Transform scannedPlayer)
+    {
+        float scaledPlayerHeight = playerHeight * playerMovment.transform.localScale.y;
+
+        middlePosition = scannedPlayer.position;
+        topPosition = scannedPlayer.position + Vector3.up * (scaledPlayerHeight / 2 - topError);
+        bottomPosition = scannedPlayer.position + Vector3.down * (scaledPlayerHeight / 2 - topError);
+        rightPosition = scannedPlayer.position + -Vector3.Cross(Vector3.up, transform.position - scannedPlayer.position).normalized * sideError;
+        leftPosition = scannedPlayer.position + Vector3.Cross(Vector3.up, transform.position - scannedPlayer.position).normalized * sideError;
+
+        Vector3 myForward = -(scannedPlayer.position - transform.position).normalized;
+        myForward.y = 0;
+
+        forwardPosition = scannedPlayer.position + myForward * sideError;
+        backPosition = scannedPlayer.position +   -myForward * sideError;
+
+    }
+    private void Scan()
     {  
         if (Time.time - lastScanTime < scanIntervals)
             return;
@@ -52,18 +75,23 @@ public class VisionEnemyAI : MonoBehaviour
         lastScanTime = Time.time;
 
         if (colliders[0] is null)
+        {
+            Debug.LogError("There is no Player in enemy sight.");
             return;
+        }
 
-        scannedPlayer = colliders[0].transform;
-        scaledPlayerHeight = playerHeight * playerMovment.transform.localScale.y;
+        Transform scannedPlayer = colliders[0].transform;
+        UpdatePlayerBodyPartsPositions(scannedPlayer);
 
-        bool isBlockedMiddle = Physics.Linecast(eyeLevel.position, scannedPlayer.position, blockers);
-        bool isBlockedTop = Physics.Linecast(eyeLevel.position, scannedPlayer.position + Vector3.up * (scaledPlayerHeight/2 - topError), blockers);
-        bool isBlockedBottom = Physics.Linecast(eyeLevel.position, scannedPlayer.position + Vector3.down * (scaledPlayerHeight / 2 - topError), blockers);
-        bool isBlockedLeft = Physics.Linecast(eyeLevel.position, scannedPlayer.position + Vector3.left * sideError, blockers);
-        bool isBlockedRight = Physics.Linecast(eyeLevel.position, scannedPlayer.position + Vector3.right * sideError, blockers);
+        bool isBlockedMiddle = Physics.Linecast(eyeLevel.position, middlePosition, blockers);
+        bool isBlockedTop = Physics.Linecast(eyeLevel.position, topPosition, blockers);
+        bool isBlockedBottom = Physics.Linecast(eyeLevel.position, bottomPosition, blockers);
+        bool isBlockedLeft = Physics.Linecast(eyeLevel.position, leftPosition, blockers);
+        bool isBlockedRight = Physics.Linecast(eyeLevel.position, rightPosition, blockers);
+        bool isBlockedForward = Physics.Linecast(eyeLevel.position, forwardPosition, blockers);
+        bool isBlockedBack = Physics.Linecast(eyeLevel.position, backPosition, blockers);
 
-        bool blocked = isBlockedMiddle && isBlockedTop && isBlockedBottom && isBlockedLeft && isBlockedRight;
+        bool blocked = isBlockedMiddle && isBlockedTop && isBlockedBottom && isBlockedLeft && isBlockedRight && isBlockedForward && isBlockedBack;
 
         targerInVision = (count > 0) && (!blocked);
 
@@ -77,11 +105,22 @@ public class VisionEnemyAI : MonoBehaviour
             return;
 
         Gizmos.color = Color.blue;
-        Gizmos.DrawLine(eyeLevel.position, scannedPlayer.position);
-        Gizmos.DrawLine(eyeLevel.position, scannedPlayer.position + Vector3.up * (scaledPlayerHeight / 2 - topError));
-        Gizmos.DrawLine(eyeLevel.position, scannedPlayer.position + Vector3.down * (scaledPlayerHeight / 2 - topError));
-        Gizmos.DrawLine(eyeLevel.position, scannedPlayer.position + Vector3.left * sideError);
-        Gizmos.DrawLine(eyeLevel.position, scannedPlayer.position + Vector3.right * sideError);
+        Gizmos.DrawLine(eyeLevel.position, middlePosition);
+        Gizmos.DrawLine(eyeLevel.position, topPosition);
+        Gizmos.DrawLine(eyeLevel.position, bottomPosition);
+        Gizmos.DrawLine(eyeLevel.position, leftPosition);
+        Gizmos.DrawLine(eyeLevel.position, rightPosition);
+        Gizmos.DrawLine(eyeLevel.position, forwardPosition);
+        Gizmos.DrawLine(eyeLevel.position, backPosition);
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(middlePosition,0.01f);
+        Gizmos.DrawSphere(topPosition, 0.01f);
+        Gizmos.DrawSphere(bottomPosition, 0.01f);
+        Gizmos.DrawSphere(leftPosition, 0.01f);
+        Gizmos.DrawSphere(rightPosition, 0.01f);
+        Gizmos.DrawSphere(forwardPosition, 0.01f);
+        Gizmos.DrawSphere(backPosition, 0.01f);
 
     }
 }
