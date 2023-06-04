4423aa212d3e16d25dc0ac5cc91d2ea3dbae651c
sztynksior <szszedel@gmail.com>
2023-06-05 00:08:25 +0200

    Add DataPersistanceManager object

--- Assets/__Scripts/UI/OptionsManager.cs
+++ Assets/__Scripts/UI/OptionsManager.cs
@@ -5,10 +5,22 @@ using UnityEngine.UI;
 
 public class OptionsManager : MonoBehaviour, IDataPersistence
 {
+    [Header("UI")]
     [SerializeField] Slider _masterVolumeSlider;
     [SerializeField] Slider _SFXVolumeSlider;
     [SerializeField] Slider _musicVolumeSlider;
     [SerializeField] Slider _dialogsVolumeSlider;
+    [SerializeField] Button _applyChangesButton;
+
+    private void Start()
+    {
+        AssignUIButtons();
+    }
+
+    private void AssignUIButtons()
+    {
+        _applyChangesButton.onClick.AddListener(OnApplyChangesButtonClick);
+    }
 
     public void LoadData(GameData data)
     {
@@ -17,11 +29,16 @@ public class OptionsManager : MonoBehaviour, IDataPersistence
         this._musicVolumeSlider.value = data.MusicVolume;
         this._dialogsVolumeSlider.value = data.DialogsVolume;
     }
-    public void SaveData(GameData data)
+    public void SaveData(ref GameData data)
     {
         data.MasterVolume = this._masterVolumeSlider.value;
         data.SFXVolume = this._SFXVolumeSlider.value;
         data.MusicVolume = this._musicVolumeSlider.value;
         data.DialogsVolume = this._dialogsVolumeSlider.value;
+    }
+
+    private void OnApplyChangesButtonClick()
+    {
+
     }
 }
