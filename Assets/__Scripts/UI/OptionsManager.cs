b340f976bf2055238c0b2290dbb1a5775c3b20e1
sztynksior <szszedel@gmail.com>
2023-06-04 23:49:45 +0200

    Add options manager

---
+++ Assets/__Scripts/UI/OptionsManager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionsManager : MonoBehaviour
+{
+    [SerializeField] Slider _masterVolumeSlider;
+    [SerializeField] Slider _SFXVolumeSlider;
+    [SerializeField] Slider _musicVolumeSlider;
+    [SerializeField] Slider _dialogsVolumeSlider;
+}
