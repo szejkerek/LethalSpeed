0b216a668540b23f7eded7c2db7189b3ed7cff99
sztynksior <szszedel@gmail.com>
2023-06-07 13:04:42 +0200

    Implement SaveData method in JsonDataService

--- Assets/__Scripts/DataPersistence/JsonDataService.cs
+++ Assets/__Scripts/DataPersistence/JsonDataService.cs
@@ -1,18 +1,41 @@
+using Newtonsoft.Json;
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
-public class JsonDataService : MonoBehaviour
+public class JsonDataService : IDataService
 {
-    // Start is called before the first frame update
-    void Start()
+    public bool SaveData<T>(string RelativePath, T Data, bool Encrypted)
     {
-        
-    }
+        string path = Application.persistentDataPath + RelativePath;
 
-    // Update is called once per frame
-    void Update()
+        try
+        {
+            if (File.Exists(path))
+            {
+                Debug.Log("Data exists. Deleting old file and writing new one!");
+                File.Delete(path);
+            }
+            else
+            {
+                Debug.Log("Writing file for the first time!");
+            }
+            using FileStream stream = File.Create(path);
+            stream.Close();
+            File.WriteAllText(path, JsonConvert.SerializeObject(Data));
+            return true;
+        }
+        catch(Exception e)
+        {
+            Debug.LogError($"Unable to save data due to: {e.Message} {e.StackTrace}");
+            return false;
+        }
+
+    }
+    public T LoadData<T>(string RelativePath, bool Encrypted)
     {
-        
+        throw new System.NotImplementedException();
     }
 }
