65cd3740716dbb2467da7f72239aba4c2fd5cb63
sztynksior <szszedel@gmail.com>
2023-06-07 12:51:54 +0200

    Implement IDataService interface

--- Assets/__Scripts/DataPersistence/IDataService.cs
+++ Assets/__Scripts/DataPersistence/IDataService.cs
@@ -2,17 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class IDataService : MonoBehaviour
+public interface IDataService
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    bool SaveData<T>(string RelativePath, T Data, bool Encrypted);
+    T LoadData<T>(string RelativePath, bool Encrypted);
 }
