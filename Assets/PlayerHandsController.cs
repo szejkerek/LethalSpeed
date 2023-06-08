4ec7539004d7823f07b24c7ab590c680fdebc643
Bartłomiej Gordon <bartekk.gordon@gmail.com>
2023-06-08 19:34:14 +0200

    Move LockableAnimations to different class

--- Assets/PlayerHandsController.cs
+++ Assets/PlayerHandsController.cs
@@ -4,38 +4,6 @@ using System.Collections.Generic;
 using UnityEditor.Animations;
 using UnityEngine;
 
-public class LockableAnimation
-{
-    public readonly int AnimationHash;
-    public float ClipTime = 0;
-    public bool AttackAnimation = false;
-    public string _animationName;
-
-    private Animator _animator;
-
-    public LockableAnimation(string animationName, Animator animator, bool AttackAnimation = false)
-    {
-        _animationName = animationName;
-        _animator = animator;
-        this.AttackAnimation = AttackAnimation;
-        AnimationHash = Animator.StringToHash(animationName);
-        UpdateAnimClipTimes();
-    }
-
-    public void UpdateAnimClipTimes()
-    {
-        AnimationClip[] clips = _animator.runtimeAnimatorController.animationClips;
-        foreach (AnimationClip clip in clips)
-        {
-            if (clip.name == _animationName)
-            {
-                ClipTime = clip.length;
-                break;
-            }
-        }
-    }
-}
-
 public class PlayerHandsController : MonoBehaviour
 {
     private LockableAnimation Idle;
