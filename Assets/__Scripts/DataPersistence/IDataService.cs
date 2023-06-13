0faf2731dcb1e24cfeb7b6f9e1756ea2bea30c0b
sztynksior <szszedel@gmail.com>
2023-06-13 12:13:49 +0200

    Add InistializeData method in IDataService

--- Assets/__Scripts/DataPersistence/IDataService.cs
+++ Assets/__Scripts/DataPersistence/IDataService.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public interface IDataService
 {
+    void InitializeData<T>(string RelativePath, T Data, bool Encrypted);
     void SaveData<T>(string RelativePath, T Data, bool Encrypted);
     T LoadData<T>(string RelativePath, bool Encrypted);
 }
