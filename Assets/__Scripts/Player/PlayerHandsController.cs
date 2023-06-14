7d4c3a36cb7490a48ce4aa759034e13fd08cde88
Bartłomiej Gordon <bartekk.gordon@gmail.com>
2023-06-14 16:17:43 +0200

    Create death animation

--- Assets/__Scripts/Player/PlayerHandsController.cs
+++ Assets/__Scripts/Player/PlayerHandsController.cs
@@ -7,12 +7,14 @@ using UnityEngine;
 public class PlayerHandsController : MonoBehaviour
 {
     private LockableAnimation Idle;
+    private LockableAnimation Death;
     private List<LockableAnimation> Attacks;
 
     [SerializeField] Animator _rightArmAnimator;
     float _rightLockedTill;
     private LockableAnimation _rightCurrentState;
     bool _attacked = false;
+    bool _death = false;
 
     PlayerWeapon _playerWeapon;
 
@@ -20,18 +22,22 @@ public class PlayerHandsController : MonoBehaviour
     {
         _playerWeapon = GetComponent<PlayerWeapon>();
         Idle = new LockableAnimation("WeaponIdle", _rightArmAnimator);
-        Attacks = new List<LockableAnimation>();
-        Attacks.Add(new LockableAnimation("WeaponAttack1", _rightArmAnimator, AttackAnimation: true));
-        Attacks.Add(new LockableAnimation("WeaponAttack2", _rightArmAnimator, AttackAnimation: true));
+        Death = new LockableAnimation("WeaponDeath", _rightArmAnimator);
+        Attacks = new List<LockableAnimation>() { 
+            new LockableAnimation("WeaponAttack1", _rightArmAnimator, AttackAnimation: true),
+            new LockableAnimation("WeaponAttack2", _rightArmAnimator, AttackAnimation: true) };
+
         _rightLockedTill = 0;
         _rightCurrentState = Idle;
         _playerWeapon.onAttack += () => _attacked = true;
+        Player.onPlayerGetHit += () => _death = true;
     }
 
     private void Update()
     {
         var state = GetState();
         _attacked = false;
+        _death = false;
 
         if (state == _rightCurrentState) return;
         _rightArmAnimator.Play(state.AnimationHash, 0);
@@ -43,11 +49,12 @@ public class PlayerHandsController : MonoBehaviour
         if (Time.time < _rightLockedTill) return _rightCurrentState;
 
         if (_attacked) return LockState(Attacks.PickRandomElement());
+        if (_death) return LockState(Death, float.MaxValue);
         return Idle;
 
-        LockableAnimation LockState(LockableAnimation state)
+        LockableAnimation LockState(LockableAnimation state, float timer = 0.1f)
         {
-            _rightLockedTill = Time.time + state.ClipTime + 0.1f;
+            _rightLockedTill = Time.time + state.ClipTime + timer;
             return state;
         }
     }
