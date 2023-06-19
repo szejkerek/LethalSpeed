254f023280a89cac58661276de9bb138faa3488f
Bartłomiej Gordon <bartekk.gordon@gmail.com>
2023-06-19 17:48:54 +0200

    Enemies unforces of Player on his/her/their death

---
+++ Assets/__Scripts/Systems/EnemyManager.cs
@@ -0,0 +1,48 @@
+using System;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    public static event Action OnAllEnemiesKilled;
+
+    Enemy[] allEnemies;
+
+    int _enemyOverallCount = 0;
+    int _enemyCurrentCount;
+    public int EnemyOverallCount => _enemyOverallCount;
+    public int EnemyCurrentCount => _enemyCurrentCount;
+
+    private void Awake()
+    {
+        allEnemies = FindObjectsByType<Enemy>(FindObjectsSortMode.InstanceID);
+        _enemyOverallCount = allEnemies.Length;
+        _enemyCurrentCount = _enemyOverallCount;
+        Enemy.OnEnemyDeath += HandleEnemyKilled;
+        Player.onPlayerGetHit += HandlePlayerKilled;
+    }
+
+    private void OnDestroy()
+    {
+        Enemy.OnEnemyDeath -= HandleEnemyKilled;
+        Player.onPlayerGetHit -= HandlePlayerKilled;
+    }
+
+    private void HandlePlayerKilled()
+    {
+        foreach (Enemy enemy in allEnemies)
+        {
+            enemy.StateMachine.CurrentState.SwitchState(enemy.StateMachine.StatesFactory.Patrol());
+            enemy.AimAtTargetRigController.TurnOffRig(2f);
+        }
+    }
+
+    void HandleEnemyKilled()
+    {
+        _enemyCurrentCount--;
+        if(_enemyCurrentCount <= 0)
+        {
+            _enemyCurrentCount = 0;           
+            OnAllEnemiesKilled?.Invoke();
+        }
+    }
+}
