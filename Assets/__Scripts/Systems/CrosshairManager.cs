c708cf886eb6390663e9a1351878f75e83730ed9
Bartłomiej Gordon <bartekk.gordon@gmail.com>
2023-06-20 00:20:45 +0200

    Show dash cooldown on crosshair

--- Assets/__Scripts/Systems/CrosshairManager.cs
+++ Assets/__Scripts/Systems/CrosshairManager.cs
@@ -12,12 +12,16 @@ public class CrosshairManager : MonoBehaviour
     [SerializeField] Image _outline;
     
     [SerializeField] float _hitmarkerTimer;
+    [Range(0f,1f)]
+    [SerializeField] float _outlineAlphaOnCooldown = 0.45f;
 
     private PlayerMovement _playerMovement;
+    float _dashCooldown;
 
     private void Awake()
     {
         _playerMovement = FindObjectOfType<PlayerMovement>();
+        _dashCooldown = _playerMovement.DashProps.DashCooldown;
         PlayerWeapon.onHitRegistered += ShowHitmarker;
         PlayerMovement.OnPlayerDash += HandleJustDashed;
         PlayerMovement.OnPlayerDashRestored += HandleDashRestore;
@@ -39,10 +43,31 @@ public class CrosshairManager : MonoBehaviour
     void HandleJustDashed()
     {
         _outline.fillAmount = 0;
+        Color c = _outline.color;
+        c.a = _outlineAlphaOnCooldown;
+        _outline.color = c;
+
+        StartCoroutine(nameof(FillOutlineOverTime));
     }
 
     void HandleDashRestore()
     {
+        StopCoroutine(nameof(FillOutlineOverTime));
+        Color c = _outline.color;
+        c.a = 1f;
+        _outline.color = c;
+        _outline.fillAmount = 1;
+    }
+
+    private IEnumerator FillOutlineOverTime()
+    {
+        float elapsedTime = 0;
+        while (_outline.fillAmount < 1)
+        {
+            elapsedTime += Time.deltaTime;
+            _outline.fillAmount = Mathf.Clamp01(elapsedTime / _dashCooldown);
+            yield return null;
+        }
         _outline.fillAmount = 1;
     }
 
