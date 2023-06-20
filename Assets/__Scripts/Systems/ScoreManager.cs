025f0b4ec8c0211e1a4818df54dccafd2b1d625f
sztynksior <szszedel@gmail.com>
2023-06-20 16:23:25 +0200

    Implement logic for setting finish time

--- Assets/__Scripts/Systems/ScoreManager.cs
+++ Assets/__Scripts/Systems/ScoreManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -14,6 +15,7 @@ public class ScoreManager : MonoBehaviour
     {
         IDataService dataService = new JsonDataService();
         _scoreSavingManager = new SavingManager<ScoreData> (dataService, InitializeDefaultScoreData(), "/score-data.json", _encryptionEnabled);
+        LoadData();
     }
 
     private ScoreData InitializeDefaultScoreData()
@@ -25,5 +27,49 @@ public class ScoreManager : MonoBehaviour
             scoreData.BestTimesForLevels.Add(sceneIndex, 0f);
         }
         return scoreData;
+    }
+
+    public void SetBetterTime(int sceneIndex, float time)
+    {
+        SetBetterTime((SceneBuildIndexes)sceneIndex, time);
+    }
+
+    public void SetBetterTime(SceneBuildIndexes sceneIndex, float time)
+    {
+        if(CompareTimes(sceneIndex, time) < 0)
+        {
+            SetNewTime(sceneIndex, time);
+        }
+    }
+
+    public void SetNewTime(int sceneIndex, float time)
+    {
+        SetNewTime((SceneBuildIndexes)sceneIndex, time);
+    }
+
+    public void SetNewTime(SceneBuildIndexes sceneIndex, float time)
+    {
+        _scoreData.BestTimesForLevels[sceneIndex] = time;
+        SaveData();
+    }
+
+    public float CompareTimes(int sceneIndex, float timeToCompare)
+    {
+        return CompareTimes((SceneBuildIndexes)sceneIndex, timeToCompare);
+    }
+
+    public float CompareTimes(SceneBuildIndexes sceneIndex ,float timeToCompare)
+    {
+        return _scoreData.BestTimesForLevels[sceneIndex] - timeToCompare;
+    }
+
+    private void LoadData()
+    {
+        _scoreData = _scoreSavingManager.DeserializeJson();
+    }
+
+    private void SaveData()
+    {
+        _scoreSavingManager.SerializeJson(_scoreData);
     }
 }
