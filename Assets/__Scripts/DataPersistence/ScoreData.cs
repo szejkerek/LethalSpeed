69b66ab52d5fbb32e9f5775e741c112a0ad63f91
sztynksior <szszedel@gmail.com>
2023-06-22 01:20:14 +0200

    Create serializable class for storing score data

--- Assets/__Scripts/DataPersistence/ScoreData.cs
+++ Assets/__Scripts/DataPersistence/ScoreData.cs
@@ -4,11 +4,29 @@ using UnityEngine;
 
 public class ScoreData : ISerializableClass
 {
-    public Dictionary<SceneBuildIndexes, float> BestTimesForLevels;
+    public Dictionary<SceneBuildIndexes, SceneScore> SceneRecords;
 
-    public ScoreData() { }
-    public ScoreData(Dictionary<SceneBuildIndexes, float> bestTimesForLevels)
+    public ScoreData() 
     {
-        BestTimesForLevels = bestTimesForLevels;
+        SceneRecords = new Dictionary<SceneBuildIndexes, SceneScore>();
+    }
+}
+
+public class SceneScore
+{
+    public float BestTime;
+    public bool WasSet;
+
+    public SceneScore() {}
+
+    public SceneScore(float bestTime, bool wasSet)
+    {
+        BestTime = bestTime;
+        WasSet = wasSet;
+    }
+
+    public bool isTimeNewRecord(float newTime)
+    {
+        return newTime < BestTime;
     }
 }
